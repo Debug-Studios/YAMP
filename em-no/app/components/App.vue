6b6f6bb4c9cbd092d0d543c6c4d320e7de663eb8
Prajjwal <prajjwaldimri@hotmail.com>
2018-11-18 20:21:40 +0530

    Title change of actionbar

--- em-no/app/components/App.vue
+++ em-no/app/components/App.vue
@@ -1,6 +1,6 @@
 <template>
     <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <ActionBar title="EM-NO!"/>
         <GridLayout columns="*" rows="*">
             <Label class="message" :text="msg" col="0" row="0"/>
         </GridLayout>
